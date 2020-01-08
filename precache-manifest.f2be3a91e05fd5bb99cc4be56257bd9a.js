6f7a3cf1207a0bb2d48e70751c5ce7d2f77df9fd
Miguel Cabrera <miguelach30@gmail.com>
2020-01-08 11:59:24 -0500

    Updates

---
+++ precache-manifest.f2be3a91e05fd5bb99cc4be56257bd9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "533b5d5e99dedb32b38fbcd06efe99d1",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "39d2d8da98f8d2cfdb0a",
+    "url": "/deploy-react/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "8e2c96b0c900ab9f3e03",
+    "url": "/deploy-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "39d2d8da98f8d2cfdb0a",
+    "url": "/deploy-react/static/js/2.1c6c948d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/deploy-react/static/js/2.1c6c948d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8e2c96b0c900ab9f3e03",
+    "url": "/deploy-react/static/js/main.02f3b2ad.chunk.js"
+  },
+  {
+    "revision": "ba4f6e82f4e491f6821a",
+    "url": "/deploy-react/static/js/runtime-main.817a3ac7.js"
+  }
+]);
